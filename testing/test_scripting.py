2d1518473748ace105825b54bc729793edcae103
Robert  Wilson <robertwilson190@gmail.com>
2023-06-05 09:39:34 +0100

    switch to importing reqs

--- testing/test_scripting.py
+++ testing/test_scripting.py
@@ -1,5 +1,14 @@
-import nctoolkit as nc
+# import nctoolkit as nc
 
-ds = nc.open_data("data/sst.mon.mean.nc")
-ds.tmean()
-ds.run()
+# ds = nc.open_data("data/sst.mon.mean.nc")
+# ds.tmean()
+# ds.run()
+
+import xarray
+import netCDF4
+import hvplot
+import panel
+import dill
+import python-dateutil
+import matplotlib
+
