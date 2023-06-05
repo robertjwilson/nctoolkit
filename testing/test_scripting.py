fd57ead8bb4d3e6c85ec8245c3a76bf70b7c8f35
Robert  Wilson <robertwilson190@gmail.com>
2023-06-05 10:17:58 +0100

    try subprocess

--- testing/test_scripting.py
+++ testing/test_scripting.py
@@ -7,6 +7,5 @@ session_info = dict()
 if __name__ == "__main__":
 
     with multiprocessing.Manager() as manager:
-    #mgr = Manager()
-    nc_safe_par = manager.list()
+        nc_safe_par = manager.list()
 nc_safe = list()
