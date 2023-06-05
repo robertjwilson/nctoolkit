333b9c0b9661bfce83078fae5978980b00dacbc8
Robert  Wilson <robertwilson190@gmail.com>
2023-06-05 11:50:20 +0100

    try freeze support

--- nctoolkit/__init__.py
+++ nctoolkit/__init__.py
@@ -1,43 +1,49 @@
-from nctoolkit.api import (
-    open_data,
-    open_geotiff,
-    from_xarray,
-    merge,
-    cor_time,
-    cor_space,
-    options,
-    DataSet,
-    open_thredds,
-    open_url,
-)
-
-from nctoolkit.unify import unify
-from nctoolkit.shape import open_shape
-
-from nctoolkit.validator import validator
-from nctoolkit.matchpoint import open_matchpoint
-
-
-from nctoolkit.cleanup import cleanup, clean_all, deep_clean, temp_check
 
 import atexit
 import signal
 
-atexit.register(clean_all)
+if __name__ == '__main__':
+                freeze_support()
 
-signal.signal(signal.SIGTERM, clean_all)
+if True:
+    from nctoolkit.api import (
+        open_data,
+        open_geotiff,
+        from_xarray,
+        merge,
+        cor_time,
+        cor_space,
+        options,
+        DataSet,
+        open_thredds,
+        open_url,
+    )
+    
+    from nctoolkit.unify import unify
+    from nctoolkit.shape import open_shape
+    
+    from nctoolkit.validator import validator
+    from nctoolkit.matchpoint import open_matchpoint
+    
+    
+    from nctoolkit.cleanup import cleanup, clean_all, deep_clean, temp_check
 
-from nctoolkit.create_ensemble import create_ensemble
-from nctoolkit.session import session_files
-from nctoolkit.show import nc_variables, nc_years, nc_months, nc_times
 
-from nctoolkit.utils import validate_version, cdo_version
-from nctoolkit.session import session_info
-from nctoolkit.mp_adders import match_points
+    from nctoolkit.create_ensemble import create_ensemble
+    from nctoolkit.session import session_files
+    from nctoolkit.show import nc_variables, nc_years, nc_months, nc_times
 
-session_info["cdo"] = cdo_version()
+    from nctoolkit.utils import validate_version, cdo_version
+    from nctoolkit.session import session_info
+    from nctoolkit.mp_adders import match_points
 
-validate_version()
+    session_info["cdo"] = cdo_version()
+
+    validate_version()
+    atexit.register(clean_all)
+
+    signal.signal(signal.SIGTERM, clean_all)
+
 
 try:
     from importlib.metadata import version as _version
